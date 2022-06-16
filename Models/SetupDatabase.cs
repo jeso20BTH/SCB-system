f50129c1fe80dc0499203739fc5e5a1a9ee2a863
jeso20BTH <jeso20@student.bth.se>
2022-06-16 11:21:53 +0100

    Database setup.

---
+++ Models/SetupDatabase.cs
@@ -0,0 +1,56 @@
+﻿using FileHandler;
+using MongoDB.Driver;
+using SCBHandeler;
+
+namespace SCBCompilerAPI.Models
+{
+    public class SetupDatabase
+    {
+        private readonly IMongoCollection<Municipality> _municipalitiesService;
+
+        public SetupDatabase()
+        {
+            var handler = new FileReader("appsettings.json");
+            var file = handler.readFile();
+            var mongoClient = new MongoClient(file["SCBDatabase"]["ConnectionString"].ToString());
+
+            var mongoDatabase = mongoClient.GetDatabase(file["SCBDatabase"]["DatabaseName"].ToString());
+
+            _municipalitiesService = mongoDatabase.GetCollection<Municipality>(file["SCBDatabase"]["MunicipalitiesCollectionName"].ToString());
+        }
+
+        public async void populate()
+        {
+            var dataset = await _municipalitiesService.Find(_ => true).ToListAsync();
+
+            if (dataset.Count() == 0)
+            {
+                var handler = new APIHandler();
+                var codes = handler.getMunicipalityCodes();
+                var data = handler.getStatistics();
+
+                foreach (var item in data.Keys)
+                {
+                    var statistics = new Dictionary<string, Dictionary<string, string>>();
+                    string name = codes[item];
+
+                    for (int i = 2016; i < 2021; i++)
+                    {
+                        statistics.Add(
+                            i.ToString(), new Dictionary<string, string>
+                            {
+                            {"1", data[item][i.ToString()]["1"]},
+                            {"2", data[item][i.ToString()]["2"]}
+                            }
+                        );
+                    }
+                    var newMunicipality = new Municipality(item, name, statistics);
+
+                    await _municipalitiesService.InsertOneAsync(newMunicipality);
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ Services/MunicipalitiesServices.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using SCBCompilerAPI.Models;
+
+namespace SCBCompilerAPI.Services
+{
+    public class MunicipalitiesServices
+    {
+        private readonly IMongoCollection<Municipality> _municipalitiesCollection;
+
+        public MunicipalitiesServices(
+            IOptions<DatabaseSettings> DatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                DatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                DatabaseSettings.Value.DatabaseName);
+
+            _municipalitiesCollection = mongoDatabase.GetCollection<Municipality>(
+                DatabaseSettings.Value.MunicipalitiesCollectionName);
+        }
+
+        public async Task<List<Municipality>> GetAsync() =>
+            await _municipalitiesCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Municipality?> GetAsync(string id) =>
+            await _municipalitiesCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Municipality newMunicipality) =>
+            await _municipalitiesCollection.InsertOneAsync(newMunicipality);
+
+        public async Task UpdateAsync(string id, Municipality updatedMunicipality) =>
+            await _municipalitiesCollection.ReplaceOneAsync(x => x.Id == id, updatedMunicipality);
+
+        public async Task RemoveAsync(string id) =>
+            await _municipalitiesCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
