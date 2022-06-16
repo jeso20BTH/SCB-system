50545af3a17c79d37ebc6382373a72fd9bc1d028
jeso20BTH <jeso20@student.bth.se>
2022-06-16 11:29:56 +0100

    Backend part of user client.

---
+++ Controllers/MunicipalityController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace UserClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MunicipalityController : ControllerBase
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private readonly ILogger<MunicipalityController> _logger;
+
+        public MunicipalityController(ILogger<MunicipalityController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            var data = client.GetStringAsync("https://localhost:7165/api/Municipality").Result;
+            return data;
+        }
+    }      
+}
