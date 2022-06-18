05da25bef8a4091dee634a58f2a436760c1ea10a
jeso20BTH <jeso20@student.bth.se>
2022-06-18 21:45:27 +0100

    Added documentation about classes and their methods.

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './Home.css';
 
+/** React component that is used to handle the home page content.*/
 export class Home extends Component {
     static displayName = Home.name;
 
+    /**
+     * A method that renders the content of the homepage.    
+     * @returns {Component}         The content of the homepage.
+     */
     render () {
         return (
             <div>
                 <h1>V&#228;lkommen</h1>
                 <div className="baby-img-div">
-                    <img className="baby-img" src="https://images.unsplash.com/photo-1555252333-9f8e92e65df9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" al="baby"/>
+                    <img className="baby-img" alt="barn" src="https://images.unsplash.com/photo-1555252333-9f8e92e65df9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" al="baby"/>
                     <span className="baby-img-text">P&#229; denna sida listas statistik f&#246;r barnf&#246;dslar mellan &#229;ren 2016-2020</span>
                 </div>
             </div>
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -3,44 +3,58 @@ import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLi
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
+/** React component that is used to display the navbar of the webpage.*/
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+    static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+    //the class constructor
+    /**
+     * Set up the initial stage for the component.
+     * @param  {object} props properties sent from parent.
+     */
+    constructor (props) {
+        super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+    /**
+     * A method that toggles if the menu shal be collapsed or not.   
+     * @returns {void}
+     */
+    toggleNavbar () {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
 
-  render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-                    <NavbarBrand tag={Link} to="/">BF</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Hem</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink tag={Link} className="text-dark" to="/statistics">Statistik</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    );
-  }
+    /**
+     * A method that renders the navbar of the page.  
+     * @returns {Component}         The navbar of the page.         
+     */
+    render () {
+        return (
+            <header>
+                <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+                    <Container>
+                        <NavbarBrand tag={Link} to="/">BF</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                        <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                            <ul className="navbar-nav flex-grow">
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/">Hem</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink tag={Link} className="text-dark" to="/statistics">Statistik</NavLink>
+                                </NavItem>
+                            </ul>
+                        </Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+        );
+    }
 }
